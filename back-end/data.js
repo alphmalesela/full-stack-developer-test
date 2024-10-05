1db871eb84a3e18419ce13da8d5a3daf19ff14c8
Malesela Seabi <seabiali@gmail.com>
2024-10-05 18:26:36 +0200

    /uniqueUsers route creates uniqueUsers.js and duplicateUsers.csv

--- back-end/data.js
+++ back-end/data.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const { encryptData, decryptData } = require('./encrypt');
+let writeStream = fs.createWriteStream('duplicateUsers.csv')
 
 const saveUsers = (users) => {
     const encrypteData = encryptData(users);
@@ -13,7 +14,32 @@ const getUsers = () => {
     return JSON.parse(decrypteData);
 }
 
+const saveUniqueUsers = (users) => {
+    fs.writeFileSync("uniqueUsers.json", JSON.stringify(users), 'utf8');
+}
+
+const readUniqueUsers = () => {
+    return JSON.parse(fs.readFileSync("uniqueUsers.json", 'utf8'));
+}
+
+const writeDuplicate = (user) => {
+    let newLine = []
+    newLine.push(user.name)
+    newLine.push(user.surname)
+    newLine.push(user.duplicates)
+
+    writeStream.write(newLine.join(',') + '\n', () => { })
+}
+
+const completeDuplicates = () => {
+    writeStream.end()
+}
+
 module.exports = {
     saveUsers,
-    getUsers
+    getUsers,
+    saveUniqueUsers,
+    readUniqueUsers,
+    writeDuplicate,
+    completeDuplicates
 }
--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,10 +1,64 @@
 const express = require('express');
 const router = express.Router();
+const crypto = require('crypto');
+const { getUsers, saveUniqueUsers, writeDuplicate, completeDuplicates } = require('../data');
 
-const { getUsers } = require('../data');
 router.get('/all', async (req, res) => {
-    const users = getUsers();
-    res.json(users).status(200)
+    try {
+        const users = getUsers();
+        res.json(users).status(200)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: 'Could not fetch users' });
+    }
+})
+
+router.get('/uniqueUsers', async (req, res) => {
+    try {
+        const users = getUsers();
+        let uniqueUsers = []
+        const seen = new Set();
+        const mapUsers = new Map()
+
+        for (const user of users) {
+            const key = `${user.name}-${user.surname}`;
+            if (!seen.has(key)) {
+                const UUID = crypto.randomUUID()
+                uniqueUsers.push({
+                    id: UUID,
+                    name: user.name,
+                    surname: user.surname,
+                    department: user.department,
+                    designation: user.designation,
+                })
+                seen.add(key)
+            } else {
+                const key = `${user.name}-${user.surname}`;
+                if (mapUsers.has(key)) {
+                    const exisitng = mapUsers.get(key)
+                    mapUsers.set(key, {
+                        name: user.name,
+                        surname: user.surname,
+                        duplicates: exisitng.duplicates += 1
+                    })
+                } else {
+                    mapUsers.set(key, {
+                        name: user.name,
+                        surname: user.surname,
+                        duplicates: 1
+                    })
+                }
+            }
+        }
+        saveUniqueUsers(uniqueUsers)
+        mapUsers.forEach((user) => {
+            writeDuplicate(user)
+        });
+        completeDuplicates()
+        res.json({ messsage: "Unique users and duplicates saved" })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 module.exports = router;
