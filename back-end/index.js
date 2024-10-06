c8989fecf6b19004d73a01b0379afc1d2dfb2c4f
Malesela Seabi <seabiali@gmail.com>
2024-10-06 14:50:36 +0200

    Websocket connection Reverse strings

--- back-end/index.js
+++ back-end/index.js
@@ -4,10 +4,12 @@ require('dotenv').config()
 const authRoutes = require('./routes/auth');
 const usersRoutes = require('./routes/users');
 const nosqlRoutes = require('./routes/nosql');
+const wsRoutes = require('./routes/websocket');
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/users', usersRoutes);
 app.use('/nosql', nosqlRoutes);
+app.use('/ws', wsRoutes);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ back-end/routes/websocket.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const WebSocket = require('ws');
+
+const ws = new WebSocket('wss://challenge.sedilink.co.za:3006');
+const stringToReverse = 'Please reverse this message'
+
+ws.on('open', function open() {
+    const message = stringToReverse;
+    ws.send(message);
+    console.log('Message sent:', message);
+});
+
+ws.on('error', function (error) {
+    console.error('WebSocket error:', error);
+});
+
+ws.on('message', function incoming(data) {
+    const receivedMessage = data.toString();
+    console.log('Received:', receivedMessage);
+    let receivedMessageReversed = ""
+
+    for (var i = receivedMessage.length - 1; i >= 0; i--) {
+        receivedMessageReversed += receivedMessage[i]
+    }
+
+    if (receivedMessageReversed === stringToReverse) {
+        console.log("String reverse correctly")
+    } else {
+        console.log("String reverse incorrectly")
+    }
+})
+
+module.exports = router;
