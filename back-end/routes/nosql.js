3e060a63e0292b68c09ad808458deb0aedb5534a
Malesela Seabi <seabiali@gmail.com>
2024-10-06 14:01:13 +0200

    Mongodb seedData route and engDepReport route

---
+++ back-end/routes/nosql.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const router = express.Router();
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://seabiali:IZak88q1uRPnBMjk@cluster0.nt4rl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri);
+const { readUniqueUsers } = require('../data');
+const { toArray } = require('gsap');
+
+router.get('/seedData', async (req, res) => {
+
+    try {
+        await client.connect();
+
+        const uniqueUsers = readUniqueUsers();
+
+        const database = client.db('Experiments');
+        const users = database.collection('users');
+
+        const result = await users.insertMany(uniqueUsers);
+
+        const message = `${result.insertedCount} documents were inserted.`
+        console.log(message);
+
+        res.json({ message })
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.close();
+    }
+
+})
+
+//Engineering Department Reporting
+router.get('/engDepReport', async (req, res) => {
+
+    try {
+        await client.connect();
+
+        const database = client.db('Experiments');
+        const users = database.collection('users');
+
+        const results = await users.aggregate([
+            {
+                $match: {
+                    designation: { $in: ["MECHANIC", "MECHANIC ASSISTANT"] },
+                    $nor: [
+                        { $and: [{ name: "MICHEAL" }, { surname: "PHALANE" }] }
+                    ]
+                }
+            },
+            {
+                $group: {
+                    _id: null,
+                    total: { $sum: 1 }
+                }
+            }
+        ]).toArray()
+
+        const total = results.length > 0 ? results[0].total : 0
+
+        res.json({ total });
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.close();
+    }
+
+})
+
+
+module.exports = router;
